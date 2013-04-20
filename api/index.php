d26ba08418cfe42038c656cbb1a2096e0011cb27
Petr <petr@kle.cz>
2013-04-20 15:38:56 +0200

    Remove openshift files.

---
+++ api/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>jtv api</title>
+	<meta name="robots" content="noindex,nofollow" />
+<style type="text/css">
+body{background:#c2c2c2;margin:0;padding:0;font-family:sans-serif;}
+h1{margin:0;padding:0;background-color:#2C3238;border-bottom:solid 1px #000;display:block;}
+h1 a{display:inline-block;background:url('logo.png') no-repeat 10px 50%;text-indent:100%;white-space:nowrap;overflow:hidden;padding:10px;}
+p{padding:0 10px;}
+</style>
+</head>
+<body>
+<h1><a href="http://juggling.tv">juggling.tv</a></h1>
+
+<p>API for Android app. <a href="https://github.com/petrkle/juggling.tv">Get source code</a>.</p>
+
+</body>
+</html>
