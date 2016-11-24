25a6304a80154eb5e25f53c2b78be0bd466285e2
Petr <petr@kle.cz>
2016-11-24 19:29:20 +0100

    Remove adds

--- api/m.php
+++ api/m.php
@@ -5,31 +5,6 @@ require('func.php');
 
 $sponsors = array();
 
-array_push($sponsors, array(
-	'img' => 'http://juggling.tv/images/sponsors/star-burst.gif',
-	'link' => 'http://juggling.tv/sponsors/15'
-));
-
-array_push($sponsors, array(
-	'img' => 'http://juggling.tv/images/sponsors/orange-diabolo.gif',
-	'link' => 'http://juggling.tv/sponsors/8'
-));
-
-array_push($sponsors, array(
-	'img' => 'http://juggling.tv/images/sponsors/ball-flash.gif',
-	'link' => 'http://juggling.tv/sponsors/7'
-));
-
-array_push($sponsors, array(
-	'img' => 'http://juggling.tv/images/sponsors/supported-by.jpg',
-	'link' => 'http://juggling.tv/sponsors/17'
-));
-
-$oldversion = array(
-	'text' => 'This app is outdated. Please, upgrade.',
-	'link' => 'https://play.google.com/store/apps/details?id=info.zonglovani.jtv'
-);
-
 $info = array();
 
 array_push($info, $sponsors[array_rand($sponsors)]);
